faf783f4043b13c5e1cb7c222e1f52513e4ac5ff
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-06-26 16:30:13 +0600

    added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ea8309",
+        secondary: "#201f1f",
+      },
+      fontFamily: {
+        primary: ["Poppins", "sans-serif"],
+        secondary: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
